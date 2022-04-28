695144d567302f892bd2da1f4d68bbaa3e7362c7
amirkp <amirkp@gmail.com>
2022-04-27 20:24:31 -0500

    Monte Carlo Normal
    Parallelized over replications

---
+++ Price Estimation April 2022/NormalDist/MC/JV_DGP-Normal.jl
@@ -0,0 +1,66 @@
+function sim_data_JV_Normal(β_up::AbstractMatrix{T}, β_down::AbstractMatrix{T}, Σ_up, Σ_down, n_firms,i, flag, obs_up, obs_down) where T
+    if flag == false
+
+        up_data = Array{Float64, 2}(undef, 3, n_firms)
+        Random.seed!(1234+i)
+        up_data[1,:] = rand(Normal(Σ_up[1,1], sqrt(Σ_up[1,2])), n_firms)
+        up_data[2,:] = rand(Normal(Σ_up[2,1], sqrt(Σ_up[2,2])), n_firms)
+        up_data[3,:] = rand(Normal(Σ_up[3,1], sqrt(Σ_up[3,2])), n_firms)
+
+        Random.seed!(1234+i)
+
+        down_data = Array{Float64, 2}(undef, 3, n_firms)
+        down_data[1,:] = rand(Normal(Σ_down[1,1], sqrt(Σ_down[1,2])), n_firms)
+        down_data[2,:] = rand(Normal(Σ_down[2,1], sqrt(Σ_down[2,2])), n_firms)
+        down_data[3,:] = rand(Normal(Σ_down[3,1], sqrt(Σ_down[3,2])), n_firms)
+    elseif flag==true
+        Random.seed!(1234+i)
+        up_data = Array{Float64, 2}(undef, 3, n_firms)
+        up_data[1:2,:] = obs_up
+        up_data[3,:] = rand(Normal(Σ_up[3,1], sqrt(Σ_up[3,2])), n_firms)
+
+        Random.seed!(1234+i)
+
+        down_data = Array{Float64, 2}(undef, 3, n_firms)
+        down_data[1,:] = rand(Normal(Σ_down[1,1], sqrt(Σ_down[1,2])), n_firms)
+        down_data[2,:] = rand(Normal(Σ_down[2,1], sqrt(Σ_down[2,2])), n_firms)
+        down_data[3,:] = rand(Normal(Σ_down[3,1], sqrt(Σ_down[3,2])), n_firms)
+    end
+
+    #
+    A_mat = β_up + β_down
+    C = -1*Transpose(up_data)*A_mat*down_data #pairwise surplus
+
+    # C=rand(500,500)
+    match, up_profit_data, down_profit_data = find_best_assignment(C)
+
+    # down_match_data=  Array{T, 2}(undef, 3, n_firms)
+    down_match_data= zeros(T, 3, n_firms)
+
+    for i=1:n_firms
+        down_match_data[:,i] = down_data[:, match[i][2]]
+    end
+
+    # down_match_profit_data =  Array{T, 1}(undef, n_firms)
+    down_match_profit_data = zeros(T, 3, n_firms)
+    for i=1:n_firms
+        down_match_profit_data[i] = down_profit_data[match[i][2]]
+    end
+
+
+    up_valuation = diag(up_data'*β_up*down_match_data)
+    up_prices = up_profit_data - up_valuation
+
+    return up_data, down_match_data, up_prices
+    # , up_profit_data, down_match_profit_data
+end
+#
+#
+# Random.seed!(1234)
+#
+# b_up = [2 0.5; 1. 0.2]
+# b_down = [1 0.2; 2. 0.3]
+#
+# sig_up = [0. 1.5; 0 .6]
+# sig_down = [0. 1; 0 .3]
+# ud, dd, up, dp,down =  sim_data_LP(b_up,b_down,sig_up,sig_down,1500,2)
---
+++ Price Estimation April 2022/NormalDist/MC/MC_JV_Normal.jl
@@ -0,0 +1,200 @@
+### Monte-Carlo -- NPSML estimator -- Normally Distributed
+### Simulation using finite market approximation. 
+# package for parallel computation
+using Distributed
+using BSON
+# using FLoops
+addprocs(24)    # Cores -1 (This is for #444 Mac Pro)
+@everywhere using Optim    # Nelder-Mead Local Optimizer
+@everywhere using CMAEvolutionStrategy # Global Optimizer
+
+@everywhere begin
+    using LinearAlgebra
+    using Random
+    using Distributions
+    using BlackBoxOptim
+    using Plots
+    using Assignment
+    using BenchmarkTools
+    include("JV_DGP-Normal.jl")
+end
+
+@everywhere begin 
+    n_reps =50 # Number of replications (fake datasets)
+    n_sim = 50
+    true_pars = [-1, 1.5, .5, 2.5, 2.5, -2, 1, -1, .5 ]
+end
+
+
+@everywhere function replicate_byseed(n_rep, n_firms)
+
+    Σ_up = [ 
+        0 2.;
+        0 1.;
+        0 1.
+        ]
+
+
+    Σ_down =  [
+                0 1.5;
+                0 3.;
+                0 1.
+    ]
+
+    #      [β11u, β12u, β21u, β11u, β11d, β12d, β21u, β13u, β33d]
+
+
+    function par_gen(b)
+        bup = [
+            vcat(b[1:2],b[8])';
+            vcat(b[3:4], 0.)';
+            vcat(0 , 0, 0)'
+        ]
+
+
+        bdown = [
+            vcat(b[5], b[6],0)';
+            vcat(b[7], 0, 0)';
+            vcat(0 ,0., b[9] )'
+        ]
+
+        return bup, bdown
+    end
+
+
+    bup, bdown = par_gen(true_pars)
+    up_data, down_data, price_data =
+        sim_data_JV_Normal(bup, bdown, Σ_up, Σ_down, n_firms, 333 + n_rep, false, 0, 0)
+
+    # mean of transfers in the data
+    mu_price = mean(price_data)
+
+
+
+    # # h: vector of bandwidths
+    # # function to be minimized over the choice of h
+    # # function uses the fake data above
+    # function bcv2_fun(h, down_data, price_data)
+    #     h=abs.(h)
+    #     ll = 0.0
+    #     n_firms = length(price_data)
+    #     for i = 1:n_firms
+    #         for j=1:n_firms
+    #             if (j!=i)
+    #                 expr_1 = ((down_data[1,i]-down_data[1,j])/h[1])^2 + ((down_data[2,i]-down_data[2,j])/h[2])^2 + ((price_data[i]-price_data[j])/h[3])^2
+    #                 expr_2 = pdf(Normal(),(down_data[1,i]-down_data[1,j])/h[1]) * pdf(Normal(),((down_data[2,i]-down_data[2,j])/h[2])) * pdf(Normal(),((price_data[i]-price_data[j])/h[3]))
+    #                 ll += (expr_1 - (2*3 +4)*expr_1 + (3^2 +2*3))*expr_2
+    #             end
+    #         end
+    #     end
+    #     val = ((sqrt(2*pi))^3 * n_firms *h[1]*h[2]*h[3])^(-1) +
+    #                             ((4*n_firms*(n_firms-1))*h[1]*h[2]*h[3])^(-1) * ll
+    #     println("band: ",h," val: ", val)
+    #     return val
+    # end
+
+    # # only use a sample of size of the nsims not the total observed sample 
+    # inds = rand(1:n_firms, n_sim)
+    # # Optimize over choice of h
+    # res_bcv = Optim.optimize(x->bcv2_fun(x,down_data[1:2,inds],price_data[inds]), rand(3))
+    # # res_bcv = Optim.optimize(bcv2_fun, rand(3),BFGS(),autodiff = :forward)
+    # h = abs.(Optim.minimizer(res_bcv))
+
+    # Silverman
+    m=3
+    S=cov(hcat(down_data[1,:], down_data[2,:], price_data))
+    H_Silverman = (4/(n_sim*(m+2)))^(2/(m+4)) * S
+    @show h= sqrt.(diag(H_Silverman))
+    function loglike(b)
+        
+        bup = [
+            vcat(b[1:2],b[8])';
+            vcat(b[3:4], 0.)';
+            vcat(0 , 0, 0)'
+        ]
+
+
+        bdown = [
+            vcat(b[5], b[6],0)';
+            vcat(b[7], 0, 0)';
+            vcat(0 ,0., b[9] )'
+        ]
+    
+
+
+        solve_draw =  x->sim_data_JV_Normal(bup, bdown , Σ_up, Σ_down, n_firms, 1234+x, true, up_data[1:2,:], down_data[1:2,:])
+        sim_dat = map(solve_draw, 1:n_sim)
+        ll=0.0
+
+        for j=1:n_sim
+            pconst = mean(sim_dat[j][3])-mu_price
+            sim_dat[j][3][:] = sim_dat[j][3] .+ pconst
+        end
+        n_zeros = 0
+        for i =1:n_firms
+            like =0.
+            for j =1:n_sim
+                like+=(
+                    pdf(Normal(),((down_data[1,i] - sim_dat[j][2][1,i])/h[1]))
+                    *pdf(Normal(),((down_data[2,i] - sim_dat[j][2][2,i])/h[2]))
+                    *pdf(Normal(),((price_data[i] - sim_dat[j][3][i])/h[3]))
+                    )
+            end
+            # println("like is: ", like, " log of which is: ", log(like/(n_sim*h[1]*h[2]*h[3])))
+            if like == 0
+                # println("Like is zero!!!")
+                ll+= -n_firms
+                n_zeros += 1
+            else
+                ll+=log(like/(n_sim*h[1]*h[2]*h[3]))
+            end
+
+
+        end
+        # println("n_rep: ", n_rep," parameter: ", round.(b, digits=3), " function value: ", -ll/n_firms, " Number of zeros: ", n_zeros)
+        return -ll/n_firms
+    end
+
+
+    res_CMAE = CMAEvolutionStrategy.minimize(loglike, rand(9), 1.,
+    lower = nothing,
+    upper = nothing,
+    noise_handling = nothing,
+    callback = (object, inputs, function_values, ranks) -> nothing,
+    parallel_evaluation = false,
+    multi_threading = false,
+    verbosity = 1,
+    seed = rand(UInt),
+    maxtime = 1000,
+    maxiter = nothing,
+    maxfevals = nothing,
+    ftarget = nothing,
+    xtol = nothing,
+    ftol = 1e-3)
+
+
+
+    # Estimated parameters: 
+    return xbest(res_CMAE)
+
+
+
+end
+
+
+
+# Parameter estimates 
+
+for n_firms = 100:100:500
+    est_pars = pmap(x->replicate_byseed(x, n_firms),1:n_reps)
+
+    estimation_result = Dict()
+    push!(estimation_result, "beta_hat" => est_pars)
+    push!(estimation_result, "beta" => true_pars)
+    bson("NormalDist/MC/MC_100_nf_$n_firms.bson", estimation_result)
+end
+
+
+
+
+estimation_result
